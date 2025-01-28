35971490344591a15aea0ff776eb8c4b30a4d243
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-01-28 16:08:52 +0500

    init

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,115 @@
+class ContactModel {
+  late String name;
+  late String phoneNumber;
+  late String avatar;
+
+  ContactModel({
+    required this.name,
+    required this.avatar,
+    required this.phoneNumber,
+  });
+}
+
+List<ContactModel> contacts = [
+  ContactModel(
+    name: 'Alisher Qodirov',
+    phoneNumber: '(99) 033-01-21',
+    avatar: 'https://picsum.photos/200/300?random=1',
+  ),
+  ContactModel(
+    name: 'Gulnora Toshmatova',
+    phoneNumber: '(99) 033-02-22',
+    avatar: 'https://picsum.photos/200/300?random=2',
+  ),
+  ContactModel(
+    name: 'Farhod Rasulov',
+    phoneNumber: '(99) 033-03-23',
+    avatar: 'https://picsum.photos/200/300?random=3',
+  ),
+  ContactModel(
+    name: 'Nozim Xolmirzayev',
+    phoneNumber: '(99) 033-04-24',
+    avatar: 'https://picsum.photos/200/300?random=4',
+  ),
+  ContactModel(
+    name: 'Dilfuza Yuldasheva',
+    phoneNumber: '(99) 033-05-25',
+    avatar: 'https://picsum.photos/200/300?random=5',
+  ),
+  ContactModel(
+    name: 'Javlon Sobirov',
+    phoneNumber: '(99) 033-06-26',
+    avatar: 'https://picsum.photos/200/300?random=6',
+  ),
+  ContactModel(
+    name: 'Sevara Azimova',
+    phoneNumber: '(99) 033-07-27',
+    avatar: 'https://picsum.photos/200/300?random=7',
+  ),
+  ContactModel(
+    name: 'Rustam Karimov',
+    phoneNumber: '(99) 033-08-28',
+    avatar: 'https://picsum.photos/200/300?random=8',
+  ),
+  ContactModel(
+    name: 'Zarina Ismoilova',
+    phoneNumber: '(99) 033-09-29',
+    avatar: 'https://picsum.photos/200/300?random=9',
+  ),
+  ContactModel(
+    name: 'Bekzod Nurmatov',
+    phoneNumber: '(99) 033-10-30',
+    avatar: 'https://picsum.photos/200/300?random=10',
+  ),
+  ContactModel(
+    name: 'Madina Rahmonova',
+    phoneNumber: '(99) 033-11-31',
+    avatar: 'https://picsum.photos/200/300?random=11',
+  ),
+  ContactModel(
+    name: 'Shoxrux Bozorov',
+    phoneNumber: '(99) 033-12-32',
+    avatar: 'https://picsum.photos/200/300?random=12',
+  ),
+  ContactModel(
+    name: 'Dilbar Otaboyeva',
+    phoneNumber: '(99) 033-13-33',
+    avatar: 'https://picsum.photos/200/300?random=13',
+  ),
+  ContactModel(
+    name: 'Jamoliddin Usmonov',
+    phoneNumber: '(99) 033-14-34',
+    avatar: 'https://picsum.photos/200/300?random=14',
+  ),
+  ContactModel(
+    name: 'Feruza Xakimova',
+    phoneNumber: '(99) 033-15-35',
+    avatar: 'https://picsum.photos/200/300?random=15',
+  ),
+  // Additional Uzbek names
+  ContactModel(
+    name: 'Dilshod Abdullayev',
+    phoneNumber: '(99) 033-16-36',
+    avatar: 'https://picsum.photos/200/300?random=16',
+  ),
+  ContactModel(
+    name: 'Shaxnoza Qurbonova',
+    phoneNumber: '(99) 033-17-37',
+    avatar: 'https://picsum.photos/200/300?random=17',
+  ),
+  ContactModel(
+    name: 'Behzod Toʻxtayev',
+    phoneNumber: '(99) 033-18-38',
+    avatar: 'https://picsum.photos/200/300?random=18',
+  ),
+  ContactModel(
+    name: 'Zulfiya Mahmudova',
+    phoneNumber: '(99) 033-19-39',
+    avatar: 'https://picsum.photos/200/300?random=19',
+  ),
+  ContactModel(
+    name: 'Jamshid Safarov',
+    phoneNumber: '(99) 033-20-40',
+    avatar: 'https://picsum.photos/200/300?random=20',
+  ),
+];
